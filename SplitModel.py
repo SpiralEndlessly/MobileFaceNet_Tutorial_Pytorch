1480038a2b1e4a4ccd7f18a3f4764c819d78d1e3
David Drexel <david.drexel@mci.edu>
2022-08-30 14:54:30 +0200

    Perform more shallow network cut

--- SplitModel.py
+++ SplitModel.py
@@ -15,7 +15,7 @@ import math
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class FrontEndNet(nn.Module):
-    def __init__(self):
+    def __init__(self, latent_size):
         super(FrontEndNet, self).__init__()
         self.conv1 = Conv_block(3, 64, kernel=(3, 3), stride=(2, 2), padding=(1, 1))
         self.conv2_dw = Conv_block(64, 64, kernel=(3, 3), stride=(1, 1), padding=(1, 1), groups=64)
@@ -25,6 +25,10 @@ class FrontEndNet(nn.Module):
         self.conv_4 = Residual(128, num_block=6, groups=256, kernel=(3, 3), stride=(1, 1), padding=(1, 1))
         self.conv_45 = Depth_Wise(128, 128, kernel=(3, 3), stride=(2, 2), padding=(1, 1), groups=512)
         self.conv_5 = Residual(128, num_block=2, groups=256, kernel=(3, 3), stride=(1, 1), padding=(1, 1))
+
+        self.conv_sep = nn.Conv2d(in_channels=128, out_channels=latent_size, kernel_size=(1,1), stride=(1,1), padding=(0,0), groups=1, bias=False)
+        self.bn_sep = nn.BatchNorm2d(latent_size)
+        self.relu = nn.ReLU()
     
     def forward(self, x):
         out = self.conv1(x)
@@ -35,25 +39,23 @@ class FrontEndNet(nn.Module):
         out = self.conv_4(out)
         out = self.conv_45(out)
         out = self.conv_5(out)
+
+        out = self.conv_sep(out)
+        out = self.bn_sep(out)
+        out = self.relu(out)
         
         return out
 
 class BackEndNet(nn.Module):
     def __init__(self, latent_size):
         super(BackEndNet, self).__init__()
-        self.conv_sep = nn.Conv2d(in_channels=128, out_channels=latent_size, kernel_size=(1,1), stride=(1,1), padding=(0,0), groups=1, bias=False)
-        self.bn_sep = nn.BatchNorm2d(latent_size)
-        self.relu = nn.ReLU()
         self.conv_dw = nn.Conv2d(latent_size, out_channels=latent_size, kernel_size=(7,7), stride=(1,1), padding=(0,0), groups=latent_size, bias=False)
         self.bn_dw = nn.BatchNorm2d(latent_size)
         self.conv_fin = nn.Conv2d(latent_size, out_channels=128, kernel_size=(1,1), stride=(1,1), padding=(0,0), groups=1, bias=False)
         self.flatten = nn.Flatten()
     
     def forward(self, x):
-        out = self.conv_sep(x)
-        out = self.bn_sep(out)
-        out = self.relu(out)
-        out = self.conv_dw(out)
+        out = self.conv_dw(x)
         out = self.bn_dw(out)
         out = self.conv_fin(out)
         
@@ -71,14 +73,14 @@ def LoadPartialStateDict(model, state_dict):
 
 if __name__ == '__main__':
     state_dict = torch.load('Weights/Retrained', map_location=lambda storage, loc: storage)["net_state_dict"]
-    front = FrontEndNet()
+    front = FrontEndNet(latent_size=320)
     back = BackEndNet(latent_size=320)
 
     LoadPartialStateDict(front, state_dict)
     LoadPartialStateDict(back, state_dict)
 
     dummy_input = torch.randn(1, 3, 112, 112, device=device)
-    torch.onnx.export(front, dummy_input, "MobileFaceNet_Front.onnx", input_names=["input"], output_names=["output"], do_constant_folding=True, export_params=True, opset_version=14)
+    torch.onnx.export(front, dummy_input, "MobileFaceNet_FrontShallow.onnx", input_names=["input"], output_names=["output"], do_constant_folding=True, export_params=True, opset_version=14)
 
-    dummy_input = torch.randn(1, 128, 7, 7, device=device)
-    torch.onnx.export(back, dummy_input, "MobileFaceNet_ReducedBack.onnx", input_names=["input"], output_names=["output"], do_constant_folding=True, export_params=True, opset_version=14)
+    dummy_input = torch.randn(1, 320, 7, 7, device=device)
+    torch.onnx.export(back, dummy_input, "MobileFaceNet_BackShallow.onnx", input_names=["input"], output_names=["output"], do_constant_folding=True, export_params=True, opset_version=14)
