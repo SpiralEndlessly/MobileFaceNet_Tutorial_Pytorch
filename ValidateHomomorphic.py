8e5651842d194633a352a8c348e165643e519674
David Drexel <david.drexel@mci.edu>
2022-08-30 11:44:29 +0200

    Compare tenseal and concrete validations

--- ValidateHomomorphic.py
+++ ValidateHomomorphic.py
@@ -6,9 +6,11 @@ from face_model import MobileFaceNet
 from data_set.dataloader import LFW
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+import onnxruntime
+
 if __name__ == '__main__':
     test_data_path = "C:/test_data"
-    TEST_IDS = [1, 3, 5, 7, 9, 601, 603, 605, 607, 609]
+    TEST_IDS = [1, 3, 5, 7, 9, 601, 603, 609]
 
     root = 'data_set/lfw'
     file_list = 'data_set/lfw/lfw_pair.txt'
@@ -21,21 +23,41 @@ if __name__ == '__main__':
     detect_model.load_state_dict(torch.load('Weights/Retrained', map_location=lambda storage, loc: storage)["net_state_dict"])
     print('MobileFaceNet face detection model generated')
 
+    frontend = onnxruntime.InferenceSession("MobileFaceNet_Front.onnx")
+    backend = onnxruntime.InferenceSession("MobileFaceNet_ReducedBack.onnx")
+
     detect_model.eval()
+
+    print(f"              Clear |     TenSEAL     |     Concrete")
 
     for id in TEST_IDS:
         index = (id - 1) // 2
 
-        homomorphic = np.load(os.path.join(test_data_path, f"{id:05d}_out.npy")).squeeze()
-        left = detect_model(dataset[index][0].unsqueeze(0)).detach().numpy().squeeze()
-        right = detect_model(dataset[index][2].unsqueeze(0)).detach().numpy().squeeze()
+        left_image = dataset[index][0].unsqueeze(0).detach().numpy()
+        right_image = dataset[index][2].unsqueeze(0).detach().numpy()
 
-        homomorphic_norm = np.sqrt(np.sum(np.power(homomorphic, 2)))
+        tenseal = np.load(os.path.join(test_data_path, f"{id:05d}_ten.npy")).squeeze()
+        concrete = np.load(os.path.join(test_data_path, f"{id:05d}_con.npy")).squeeze()
+        #left = detect_model(dataset[index][0].unsqueeze(0)).detach().numpy().squeeze()
+        #right = detect_model(dataset[index][2].unsqueeze(0)).detach().numpy().squeeze()
+
+        left = frontend.run(None, {"input": left_image})[0]
+        right = frontend.run(None, {"input": right_image})[0]
+
+        left = backend.run(None, {"input": left})[0].squeeze()
+        right = backend.run(None, {"input": right})[0].squeeze()
+
+        tenseal_norm = np.sqrt(np.sum(np.power(tenseal, 2)))
+        concrete_norm = np.sqrt(np.sum(np.power(concrete, 2)))
         left_norm = np.sqrt(np.sum(np.power(left, 2)))
         right_norm = np.sqrt(np.sum(np.power(right, 2)))
 
-        homomorphic_score = np.dot(homomorphic / homomorphic_norm, right / right_norm)
-        clear_score = np.dot(left / left_norm, right / right_norm)
-        self_score = np.dot(left / left_norm, homomorphic / homomorphic_norm)
 
-        print(f"{id:05d}: Clear {clear_score: .3f} \t Enc {homomorphic_score: .3f} \t Self {self_score: .3f}")
+        clear_score = np.dot(left / left_norm, right / right_norm)
+
+        tenseal_score = np.dot(tenseal / tenseal_norm, right / right_norm)
+        tenseal_similarity = np.dot(left / left_norm, tenseal / tenseal_norm)
+        concrete_score = np.dot(concrete / concrete_norm, right / right_norm)
+        concrete_similarity = np.dot(left / left_norm, concrete / concrete_norm)
+
+        print(f"{id:05d}-{id+1:05d}: {clear_score: .3f} | {tenseal_score: .3f} ({tenseal_similarity: .3f}) | {concrete_score: .3f} ({concrete_similarity: .3f})")
